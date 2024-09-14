bbfc7486d7f10d963193c4668b8991a8b4a22996
anujaandge <88617981+anujaandge@users.noreply.github.com>
2024-09-14 14:26:18 +0530

    Database connection

---
+++ databaseConnection/database.py
@@ -0,0 +1,26 @@
+import mysql.connector
+def databaseConnection():
+    return mysql.connector.connect(
+    host="localhost",
+    user='root',
+    password='Nitin@0806',
+    database='mydatabase'     #this can directly connected to the existed database
+    )
+    
+mydb=databaseConnection()
+mycursor=mydb.cursor()
+sql="CREATE TABLE users(Id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, name VARCHAR(255) NOT NULL,password VARCHAR(255) NOT NULL,mobile_number VARCHAR(15) NOT NULL,gender ENUM('Male', 'Female') NOT NULL,age INT NOT NULL,address TEXT)"
+mycursor.execute(sql)
+#mycursor.execute("CREATE TABLE customers(name VARCHAR(255), address VARCHAR(255))")  #creating table
+
+# mycursor.execute("SHOW TABLES")    #checking for tables exit in database
+# for x in mycursor:
+#     print(x)
+
+#mycursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY") #adding new column to the existing database
+
+
+
+
+
+    
